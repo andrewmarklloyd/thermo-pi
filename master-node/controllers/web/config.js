1b8f904fdbdf133d194b65785c4755e14f97c80b
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-03-18 21:35:27 -0700

    Better initialization of socket.io

--- master-node/controllers/web/config.js
+++ master-node/controllers/web/config.js
@@ -6,33 +6,32 @@ function onSignIn(googleUser) {
   socket.on('connect', function(){
     socket.emit('authentication', {id_token});
     socket.on('authenticated', function() {
-      
+      $('#master-connection').removeClass('badge-danger');
+      $('#master-connection').addClass('badge-success');
+      socket.on('desired', function(data){
+        $(`#${data.room}-desired`).html(data.temp);
+      });
+      socket.on('current', function(data){
+        $(`#${data.room}-current`).html(data.temp.toFixed(2));
+      });
+      socket.on('disconnect', function(){
+        $('#master-connection').removeClass('badge-success');
+        $('#master-connection').addClass('badge-danger');
+        ['kitchen', 'living', 'laundry'].forEach(room => {
+          $(`#${room}-connection`).removeClass('badge-success');
+          $(`#${room}-connection`).addClass('badge-danger');
+        })
+      });
     });
-    $('#master-connection').removeClass('badge-danger');
-    $('#master-connection').addClass('badge-success');
-  });
-  socket.on('desired', function(data){
-    $(`#${data.room}-desired`).html(data.temp);
-  });
-  socket.on('current', function(data){
-    $(`#${data.room}-current`).html(data.temp.toFixed(2));
-  });
-  socket.on('disconnect', function(){
-    $('#master-connection').removeClass('badge-success');
-    $('#master-connection').addClass('badge-danger');
-    ['kitchen', 'living', 'laundry'].forEach(room => {
-      $(`#${room}-connection`).removeClass('badge-success');
-      $(`#${room}-connection`).addClass('badge-danger');
-    })
-  });
-  socket.on('worker-connection', function(data) {
-    if (data.status === 'connected') {
-      $(`#${data.room}-connection`).removeClass('badge-danger');
-      $(`#${data.room}-connection`).addClass('badge-success');
-    } else if (data.status === 'disconnected') {
-      $(`#${data.room}-connection`).removeClass('badge-warning');
-      $(`#${data.room}-connection`).addClass('badge-danger');
-    }
+    socket.on('worker-connection', function(data) {
+      if (data.status === 'connected') {
+        $(`#${data.room}-connection`).removeClass('badge-danger');
+        $(`#${data.room}-connection`).addClass('badge-success');
+      } else if (data.status === 'disconnected') {
+        $(`#${data.room}-connection`).removeClass('badge-warning');
+        $(`#${data.room}-connection`).addClass('badge-danger');
+      }
+    });
   });
 }
 
